0ebbe480f86b94372bbd05b1bc1889d263641aa8
onurgumus <onur@outlook.com.tr>
2020-12-27 20:19:56 +0100

    Start Impl main page

--- src/MyPlanner.Client/Library.fs
+++
@@ -1,5 +0,0 @@
-﻿namespace MyPlanner.Client
-
-module Say =
-    let hello name =
-        printfn "Hello %s" name
---
+++ src/MyPlanner.Client/Main.fs
@@ -0,0 +1,19 @@
+﻿namespace MyPlanner.Client.Main
+
+open MyPlanner.Shared.Msg
+
+type Route = | Tasks
+
+type Page = | Tasks
+
+type ConnectionStatus =
+    | Connected
+    | Disconnected
+
+type Model =
+    { Page: Page
+      ConnectionStatus: ConnectionStatus }
+
+type Msg =
+    | ServerDisconnected
+    | Remote of ServerToClient.Msg
