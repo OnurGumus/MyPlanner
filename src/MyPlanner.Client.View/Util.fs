c6f0f0596b70aa4752ac1c22f3be84be196452b0
Onur Gumus <onurgumus@microsoft.com>
2021-03-09 17:43:15 +0100

    Fix styling

--- src/MyPlanner.Client.View/Util.fs
+++ src/MyPlanner.Client.View/Util.fs
@@ -1,24 +1,29 @@
 module MyPlanner.Client.View.Util
 
 open Feliz
+open Fable.Core
 open Fable.React.Helpers
+open Fable.Core.JsInterop
 
-let reactShadowRoot child =
-    ofImport "default" "react-shadow-root" [] child
+let reactShadowRoot props child =
+    ofImport "default" "react-shadow-root" props child
 
-let fragment props children =
-    ofImport "default" "react-dom-fragment" props children
+importAll "construct-style-sheets-polyfill"
+
+[<Global>]
+type CSSStyleSheet() = class end
+
+let sheet = new CSSStyleSheet();
+sheet?replaceSync("h1 { color: red; }");
+Browser.Dom.document?adoptedStyleSheets <- [|sheet|];
 
 let inline loadHtmlInShadow html style tag (slots: ReactElement list) =
     Interop.reactElementWithChildren
         tag
         [
-            reactShadowRoot [
-                fragment
-                    ({|
-                         dangerouslySetInnerHTML = {| __html = (style + html) |}
-                     |})
-                    []
+            reactShadowRoot {|stylesheets = [|sheet|]|} [
+
+                Html.span [prop.dangerouslySetInnerHTML (style + html)]
             ]
             slots |> ofList
         ]
