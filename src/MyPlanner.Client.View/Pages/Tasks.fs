6a46d3f425f98939c7d1dda418030da001cdd271
Onur Gumus <onurgumus@microsoft.com>
2021-03-15 08:00:00 +0100

    More html oriented

--- src/MyPlanner.Client.View/Pages/Tasks.fs
+++ src/MyPlanner.Client.View/Pages/Tasks.fs
@@ -4,11 +4,12 @@ open MyPlanner.Client
 open Pages.Tasks
 open MyPlanner.Shared.Domain
 open Feliz
-open View.Util
 open Fable.Core.JsInterop
 open MyPlanner.Client.View.Components
-open Browser.Dom
 open Browser.Types
+
+open Browser.DomExtensions
+open Browser.Dom
 
 
 ModalWindow.ensureDefined ()
@@ -23,7 +24,7 @@ let View dispatch (model: Model) =
     | [] -> Html.none
     | _ ->
 
-        let shadowRoot, setRootTag = React.useState (None)
+        let (shadowRoot:HTMLElement option), setRootTag = React.useState (None)
         let attachShadowRoot =
             prop.ref
                 (fun x ->
@@ -31,19 +32,13 @@ let View dispatch (model: Model) =
                         setRootTag (Some(x?attachShadow {|mode = "open"|})))
 
         React.useEffect
-            (fun () ->
+           ( (fun () ->
                 match shadowRoot with
                 | Some shadowRoot ->
-                    shadowRoot?innerHTML <- html
-                    let form : HTMLFormElement = !!shadowRoot?querySelector("form")
-
-                    let onSubmit (e: Event) =
-                        e.preventDefault ()
-                        shadowRoot?querySelector("#create-task-dialog")?isVisible <- false
-
-                    form.onsubmit <- onSubmit
+                    shadowRoot.innerHTML <- html
+                    shadowRoot.addEventListener("TaskFromSubmitted", fun e ->console.log e)
                  | _ ->()
-                )
+                ), [|shadowRoot|>box|])
 
         Interop.createElement "task-list" [
             attachShadowRoot
@@ -51,10 +46,6 @@ let View dispatch (model: Model) =
                 Html.div [
                     prop.slot "task-list"
                     prop.children [
-                        Html.button [
-                            prop.text "Create"
-                            prop.onClick (fun _ -> shadowRoot.Value?querySelector("#create-task-dialog")?isVisible <- true)
-                        ]
                         Html.ol [
                             for t in model.Tasks do
                                 Html.li [ prop.textf "%A" t ]
