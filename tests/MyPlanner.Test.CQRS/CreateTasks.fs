46c623eb7d7559f024905aed41ae18ddfcd8589a
Onur Gumus <onurgumus@microsoft.com>
2021-03-25 17:00:06 +0100

    Fix streaming test

--- tests/MyPlanner.Test.CQRS/CreateTasks.fs
+++ tests/MyPlanner.Test.CQRS/CreateTasks.fs
@@ -13,6 +13,7 @@ open Hocon.Extensions.Configuration
 open Akkling.Streams
 
 
+
 [<Given>]
 let ``there are no tasks in the system`` () =
 
@@ -35,9 +36,9 @@ let ``there are no tasks in the system`` () =
 
 [<When>]
 let ``I create a task`` (api: IAPI, (qapi : MyPlanner.Query.API.IAPI)) =
+    let tasks = ResizeArray()
     let sink = 
-        Sink.forEach (fun x -> 
-            printf "onurrr %A" x)
+        Sink.forEach (fun x -> tasks.Add(x))
     let k,_ = 
         qapi.Source 
         |> Source.viaMat  KillSwitch.single Keep.right 
@@ -50,7 +51,8 @@ let ``I create a task`` (api: IAPI, (qapi : MyPlanner.Query.API.IAPI)) =
           Version = version0
           Title = TaskTitle(ShortString.ofString "title")
           Description = TaskDescription(LongString.ofString "desc") }
-    |> Async.RunSynchronously
+    |> Async.RunSynchronously,
+    tasks
 
 [<Then>]
 let ``the task should be created successfully`` (result: Result<Task, string>) =
@@ -67,3 +69,4 @@ let ``I visit url /tasks`` (queryApi: MyPlanner.Query.API.IAPI) =
 let ``I should see 1 task\(s\) listed`` (tasks: Task list, commandApi: MyPlanner.Command.API.IAPI) =
     Expect.equal tasks.Length 1 "more than 1 tasks"
     commandApi.ActorApi.Stop().Wait()
+    Expect.equal tasks.Length 1 "Stream has not returned the task"
