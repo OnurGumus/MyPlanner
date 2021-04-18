3bdb68f95c904352d5317da7dde21254f750077a
Onur Gumus <onurgumus@microsoft.com>
2021-04-18 15:48:53 +0200

    layouting

--- src/MyPlanner.Client.View/Pages/Singin.fs
+++ src/MyPlanner.Client.View/Pages/Singin.fs
@@ -11,23 +11,37 @@ open MyPlanner.Client.View
 open System
 open Browser.Types
 open Fable
+open MyPlanner.Client.View.Util
 
 ModalWindow.ensureDefined ()
 
 let html : string =
     importDefault ("!!raw-loader!./_wwwroot/design/signin/index.html")
 
+let layoutCSS : string =
+    importDefault ("!!raw-loader!./_wwwroot/layout.css")
+
+
+Browser.Dom.document?adoptedStyleSheets <- [| layoutCSS |] |> Array.map createSheet
 
 [<ReactComponent>]
 let SigninView dispatch (model: Model) =
 
     let attachShadowRoot, shadowRoot = Util.useShadowRoot (html)
 
+    React.useLayoutEffect (
+        (fun _ ->
+            match shadowRoot with
+            | Some shadowRoot -> shadowRoot?adoptedStyleSheets <- [| layoutCSS |] |> Array.map createSheet
+            | _ -> ()
+            ),
+        [| shadowRoot |> box<_> |]
+    )
+
     Interop.createElement
         "signin-page"
         [
             attachShadowRoot
-            prop.children [
-
-            ]
+            prop.className "page"
+            prop.children []
         ]
